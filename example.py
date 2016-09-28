dc415b83ac8daaeab5ec3d278742b11405798444
Sebastian Scheibenzuber <sscheibenzuber@tdt.de>
2016-09-28 13:25:05 +0200

    Add device_id variable for easier interaction

--- example.py
+++ example.py
@@ -5,35 +5,38 @@
 
 import genieacs
 
+# set a device_id for the following methods
+device_id = "000149-c1500-000149014AF8"
+
 # Create a Connection object to interact with a GenieACS server
 acs = genieacs.Connection("tr069.tdt.de", ssl=True, auth=True, user="tdt", passwd="tdt")
 
 # refresh some device parameters
-acs.task_refresh_object("000149-c1500-000149014AF8", "InternetGatewayDevice.DeviceInfo.")
+acs.task_refresh_object(device_id, "InternetGatewayDevice.DeviceInfo.")
 # set a device parameter
-acs.task_set_parameter_values("000149-c1500-000149014AF8", [["InternetGatewayDevice.BackupConfiguration.FileList", "backup.cfg"]])
+acs.task_set_parameter_values(device_id, [["InternetGatewayDevice.BackupConfiguration.FileList", "backup.cfg"]])
 # get a device parameter
-acs.task_get_parameter_values("000149-c1500-000149014AF8", [["InternetGatewayDevice.BackupConfiguration.FileList"]])
+acs.task_get_parameter_values(device_id, [["InternetGatewayDevice.BackupConfiguration.FileList"]])
 # factory reset a device
-acs.task_factory_reset("000149-c1500-000149014AF8")
+acs.task_factory_reset(device_id)
 # reboot a device
-acs.task_reboot("000149-c1500-000149014AF8")
+acs.task_reboot(device_id)
 # add an object to a device
-acs.task_add_object("000149-c1500-000149014AF8", "VPNObject", [["InternetGatewayDevice.X_TDT-DE_OpenVPN"]])
+acs.task_add_object(device_id, "VPNObject", [["InternetGatewayDevice.X_TDT-DE_OpenVPN"]])
 # download a file
-acs.task_download("000149-c1500-000149014AF8", "9823de165bb983f24f782951", "Firmware.img")
+acs.task_download(device_id, "9823de165bb983f24f782951", "Firmware.img")
 # retry a faulty task
 acs.task_retry("9h4769svl789kjf984ll")
 
 
 # print all tasks of a given device
-print(acs.task_get_all("000149-c1500-000149014AF8"))
+print(acs.task_get_all(device_id))
 # search a device by its ID and print all corresponding data
-print(acs.device_get_by_id("000149-c1500-000149014AF8"))
+print(acs.device_get_by_id(device_id))
 # search a device by its MAC address and print all corresponding data
 print(acs.device_get_by_MAC("00:01:49:ff:0f:01"))
 # print 2 given parameters of a given device
-print(acs.device_get_parameters("000149-c1500-000149014AF8", "InternetGatewayDevice.DeviceInfo.SoftwareVersion,InternetGatewayDevice.X_TDT-DE_Interface.2.ProtoStatic.Ipv4.Address"))
+print(acs.device_get_parameters(device_id, "InternetGatewayDevice.DeviceInfo.SoftwareVersion,InternetGatewayDevice.X_TDT-DE_Interface.2.ProtoStatic.Ipv4.Address"))
 # delete a task
 acs.task_delete("9h4769svl789kjf984ll")
 
@@ -58,9 +61,9 @@ for gobject in object_data:
 acs.object_create_all_from_file('objects.json')
 
 # assign a tag to a device
-acs.tag_assign("000149-Kananga-P15", "tagged")
+acs.tag_assign(device_id, "tagged")
 # remove a tag from a device
-acs.tag_remove("000149-Kananga-P15", "tagged")
+acs.tag_remove(device_id, "tagged")
 
 # print all existing files in the database
 print(acs.file_get_all())
@@ -70,4 +73,4 @@ acs.file_upload("Firmware.img", "1 Firmware Upgrade Image", "123456", "r4500", "
 acs.file_delete("Firmware.img")
 
 # delete the device from the database
-acs.device_delete("000149-c1500-000149014AF8")
+acs.device_delete(device_id)
